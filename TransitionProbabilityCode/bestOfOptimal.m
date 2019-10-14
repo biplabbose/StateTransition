f366e21f5453bac0134f1e012389019475e1bcc4
Vimalathithan Devaraj <33455036+Athithan27@users.noreply.github.com>
2019-10-14 21:12:33 +0530

    redundant files and folders
    Deleted the redundant files and folders

--- TransitionProbabilityCode/bestOfOptimal.m
+++
@@ -1,9 +0,0 @@
-function bestOptimal = bestOfOptimal(result)
-% Calculates the best solution from the pareto plot based on knee point estimation
-
-[TEMP, index] = knee_pt(result.objFun(:,2), result.objFun(:,1));
-bestOptimal.fract = result.fract(index,:);
-bestOptimal.objFun1 = result.objFun(index,1);
-bestOptimal.objFun2 = result.objFun(index,2);
-
-end
